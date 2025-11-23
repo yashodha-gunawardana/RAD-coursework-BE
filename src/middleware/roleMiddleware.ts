e1325b9e3de3ce0dd246ab3f6efb34667e75823f
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-23 15:40:07 +0530

    check if user has at least one required role and some() checks if any role of the user matches allowed roles

--- src/middleware/roleMiddleware.ts
+++ src/middleware/roleMiddleware.ts
@@ -10,9 +10,13 @@ export const requiredRole = (roles: Role[]) => {
         // check if user is logged in
         if (!req.user) {
             return res.status(401).json({
-                message: "Unauthorized.."
+                message: "Unauthorized.." // user is not logged in
             })
         }
+
+        // check if user has at least one required role
+        // some() checks if any role of the user matches allowed roles
+        const hashRole = req.user.roles?.some((r: Role) => roles.includes(r))
     }    
     
     
