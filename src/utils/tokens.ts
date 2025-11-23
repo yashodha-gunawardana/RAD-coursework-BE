7e040b5adedb3b5788b9186e7e04a1924f469138
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-23 16:56:06 +0530

    oles stores the user's roles for access control

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -11,8 +11,11 @@ const JWT_SECRET = process.env.JWT_SECRET as string
 export const signAccessToken = (user: IUser): string => {
     return jwt.sign(
         {
-            // `sub` (subject) is a standard JWT claim, storing the user's unique ID
+            // `sub` (subject) is a standard JWT claim for the user ID.
             sub: user._id.toString(),
+
+            // `roles` stores the user's roles for access control
+            roles: user.roles
         }
     )
 
