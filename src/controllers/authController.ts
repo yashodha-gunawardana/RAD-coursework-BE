8ac453d51c540d4b02af85e58cfc668037834bd9
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-23 11:23:37 +0530

    validate required fields

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,7 +4,13 @@ import { User } from "../model/userModel";
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
-        const { fullname, email, password, address, phone } = req.body;
+        const { fullname, email, password, address, phone, role } = req.body;
+
+        if (!fullname || !email || !password || !address || !phone || role) {
+            return res.status(400).json({
+                message: "All fields are required.."
+            })
+        }
     } catch (err) {
 
     }
