da42661e74523f3949df41e4e2be6bec03351529
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-26 21:36:07 +0530

    define the typescript interface for event document

--- src/model/eventModel.ts
+++ src/model/eventModel.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
 export enum Type {
     WEDDING = "WEDDING",
@@ -14,4 +14,17 @@ export enum Status {
     ONGOING = "ONGOING",
     COMPLETED = "COMPLETED",
     CANCELLED = "CANCELLED"
+}
+
+export interface IEvent extends Document {
+    userId: mongoose.Types.ObjectId
+    title: string
+    type: Type
+    date: Date
+    time?: string
+    location: string
+    description?: string
+    image?: string
+    status: Status
+    createdAt: Date
 }
