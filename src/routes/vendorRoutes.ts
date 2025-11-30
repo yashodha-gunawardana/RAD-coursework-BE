518b84f639c52704af512c813171af01fd3a5089
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-30 23:04:22 +0530

    create new router instance for vendor routes

--- src/routes/vendorRoutes.ts
+++ src/routes/vendorRoutes.ts
@@ -1,1 +1,8 @@
 import { Router } from "express";
+import { getAllVendors, createVendor, updateVendor, deleteVendor } from "../controllers/vendorController";
+import { authenticate } from "../middleware/authMiddleware";
+import { requiredRole } from "../middleware/roleMiddleware";
+import { Role } from "../model/userModel";
+
+
+const router = Router();
