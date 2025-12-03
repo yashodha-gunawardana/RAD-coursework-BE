7d5ce697b90678414ad26dbfc79fb2207b1c9737
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-04 00:54:45 +0530

    ensure the event exists and prevents unathorized users from booking on someone else's event

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -10,6 +10,13 @@ export const createBooking = async (req: AuthRequest, res: Response) => {
         const { eventId, vendorId, notes } = req.body
 
         const event = await Event.findOne({ _id: eventId, userId: req.user._id })
+
+        if (!event) {
+            return res.status(404).json({
+                message: "Event not found or not owned by you.."
+            })
+        }
+        
     } catch (err) {
 
     }
