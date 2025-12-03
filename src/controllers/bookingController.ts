acf4a0ea41a58f0f213ead9175f7623e896d29f5
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-04 00:46:36 +0530

    create new booking function

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -3,3 +3,8 @@ import Event from "../model/eventModel";
 import Booking from "../model/bookingModel";
 import { AuthRequest } from "../middleware/authMiddleware";
 
+
+// new booking create function
+export const createBooking = async (req: AuthRequest, res: Response) => {
+    
+}
