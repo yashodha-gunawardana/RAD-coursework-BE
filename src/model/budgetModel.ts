3e3ee3d97a6c9fcf699e6c55a08ccb547c5bd744
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-06 23:33:41 +0530

    create a interface for user-selected extra items

--- src/model/budgetModel.ts
+++ src/model/budgetModel.ts
@@ -1,7 +1,18 @@
 import mongoose, { Document, Schema } from "mongoose";
 
+
 export enum BudgetStatus {
     DRAFT = "DRAFT",
     CONFIRMED = "CONFIRMED",
     PAID = "PAID",
+}
+
+
+// interface for user-selected extra items
+export interface IUserSelectedItems {
+    itemId: mongoose.Types.ObjectId
+    name: string
+    unitPrice: number
+    quantity: number
+    total: number
 }
