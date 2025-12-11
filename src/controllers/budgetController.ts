abc2ed3810c2477d37d1cba7daff379525e16b3c
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-11 17:18:06 +0530

    handle errors from server

--- src/controllers/budgetController.ts
+++ src/controllers/budgetController.ts
@@ -153,7 +153,9 @@ export const getBudgetId = async (req: AuthRequest, res: Response) => {
             data: budget
         })
 
-    } catch (err) {
-
+    } catch (err: any) {
+        return res.status(500).json({
+            message: err?.message
+        })
     }
 }
