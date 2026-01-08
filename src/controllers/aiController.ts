02adae1f08aa95b3c1cb8a5c95361188426ec9fd
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-08 23:07:56 +0530

    extract request body paramateres

--- src/controllers/aiController.ts
+++ src/controllers/aiController.ts
@@ -2,5 +2,13 @@ import { Request, Response } from "express";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 
+// initialize google genearive AI with api key
 const apiKey = process.env.GEMINI_API_KEY!
 const genAI = new GoogleGenerativeAI(apiKey)
+
+export const eventConsultantAI = async (req: Request, res: Response) => {
+  try {
+
+    const { eventType, eventName, question } = req.body
+  }
+}
